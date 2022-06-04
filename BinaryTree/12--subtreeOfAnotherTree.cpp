a47d58931e584c7ca1646873bd0b8778f30e8679
coderajdeep <coder.rajdeep@gmail.com>
2022-06-04 13:36:59 +0530

    leetcode 662 solution modified

--- BinaryTree/12--subtreeOfAnotherTree.cpp
+++ BinaryTree/12--subtreeOfAnotherTree.cpp
@@ -1,7 +1,60 @@
 // leetcode 572
 
+// O(n) time solution
+
+class Solution {
+public:
+    
+    void inorder(TreeNode *root, string &path) {
+        if(!root) {
+            path.push_back('e');
+            return;
+        }
+        
+        inorder(root->left, path);
+        
+        path.push_back('x');
+        path += to_string(root->val);
+        path.push_back('x');
+        
+        inorder(root->right, path);
+    }
+    
+    void preorder(TreeNode *root, string &path) {
+        if(!root) {
+            path.push_back('e');
+            return;
+        }
+        
+        path.push_back('x');
+        path += to_string(root->val);
+        path.push_back('x');
+        
+        preorder(root->left, path);
+        preorder(root->right, path);
+    }
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root && !subRoot) return true;
+        if(!root) return false;
+        
+        string t1, t2;
+        inorder(root, t1);
+        inorder(subRoot, t2);
+        if(t1.find(t2)==string::npos) return false;
+        
+        t1.clear(); t2.clear();
+        preorder(root, t1);
+        preorder(subRoot, t2);
+        if(t1.find(t2)==string::npos) return false;
+        
+        return true;
+    }
+};
+
+
 // Time O(n*m)
-// need to improve time complexity
+// Optimized to O(n)
 
 // recursive approach
 
--- BinaryTree/13--maxWidthOfBinaryTree.cpp
+++ BinaryTree/13--maxWidthOfBinaryTree.cpp
@@ -1,4 +1,5 @@
 // leetcode 662
+// ToDo 672
 // Medium level
 
 class Solution {
@@ -33,6 +34,7 @@ public:
                 
                 // this is for handling overflow condition while doing 2*index
                 // generally in sparse tree
+                
                 index = index - minIndex;
                 
                 if(t==0) {
@@ -55,7 +57,69 @@ public:
     }
 };
 
+/*
 
+[1,1,1,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,
+null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,
+null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,1,null,null,
+1,1,null,1,null,1,null,1,null,1,null,null,null,1]
+
+*/
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root) {
+            return 0;
+        }
+        
+        queue<pair<TreeNode*, long long>>q;
+        q.push({root, 0});
+        long long ans = 0;
+        
+        while(!q.empty()) {
+            
+            int t = q.size();
+            long long hi;
+            // long long lo;
+            bool low = true;
+            long minIndex;
+            
+            while(t--) {
+                auto curr = q.front();
+                q.pop();
+                TreeNode* currNode = curr.first;
+                long long index = curr.second;
+                
+                if(low) {
+                    minIndex = index;
+                    // lo = index-minIndex;
+                    // lo will always be zero
+                    low = false;
+                }
+                
+                index = index - minIndex;
+                
+                if(t==0) {
+                    hi = index;
+                }
+                
+                
+                if(currNode->left) {
+                    q.push({currNode->left, (2*index + 1)});
+                }
+                
+                if(currNode->right) {
+                    q.push({currNode->right, (2*index + 2)});
+                }
+            }
+            
+            // ans = max(ans, (hi-lo+1));
+            ans = max(ans, (hi+1));
+        }
+        return ans;
+    }
+};
 
 
 
