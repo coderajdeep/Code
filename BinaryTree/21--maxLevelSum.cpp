c5a9bce8929a37cc246db5efe37d34aef9b202fe
coderajdeep <coder.rajdeep@gmail.com>
2022-06-17 23:39:03 +0530

    Binary Tree chnage

--- BinaryTree/21--maxLevelSum.cpp
+++ BinaryTree/21--maxLevelSum.cpp
@@ -3,40 +3,30 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        if(!root) {
-            return {};
-        }
-
+        if(!root) return 0;
+        
         queue<TreeNode*>q;
         q.push(root);
-        int level = 1, maxLevel = 1, maxSum = INT_MIN;
+        int maxSum = INT_MIN, minLevel = 1, level = 1;
         
         while(!q.empty()) {
             int size = q.size();
             int sum = 0;
-            
+
             for(int i=0; i<size; ++i) {
                 TreeNode *curr = q.front();
                 q.pop();
-                
-                sum += (curr->val);
-                
-                if(curr->left) {
-                    q.push(curr->left);
-                }
-                if(curr->right) {
-                    q.push(curr->right);
-                }
+                sum += curr->val;
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
             
             if(maxSum<sum) {
                 maxSum = sum;
-                maxLevel = level;
+                minLevel = level;
             }
-            
             ++level;
         }
-        
-        return maxLevel;
+        return minLevel;
     }
 };
