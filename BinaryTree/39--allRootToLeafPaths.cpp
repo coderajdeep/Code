4054fa001456a2f74c01eb4978056a279c28d9ff
coderajdeep <coder.rajdeep@gmail.com>
2022-06-19 12:26:57 +0530

    Root to leaf all paths -- efficient solution

--- BinaryTree/39--allRootToLeafPaths.cpp
+++ BinaryTree/39--allRootToLeafPaths.cpp
@@ -1,5 +1,53 @@
 // Leetcode 257
 // Generate all root to leaf paths
+// Very similar to path sum II (leetcode 113)
+
+
+// Best solution
+class Solution {
+public:
+    
+    bool isLeaf(TreeNode *root) {
+        if(root && !root->left && !root->right) return true;
+        return false;
+    }
+    
+    void dfs(TreeNode *root, string &ds, vector<string> &ans) {
+        if(!root) return;
+        
+        string s = to_string(root->val);
+        bool flag = false;
+        
+        if(!ds.empty()) {
+            ds += "->";
+            flag = true;
+        }
+        
+        ds += s;
+        
+        if(isLeaf(root))
+            ans.push_back(ds);
+        
+        dfs(root->left, ds, ans);
+        dfs(root->right, ds, ans);
+        
+        int n = flag ? (s.size() + 2) : s.size();
+        
+        for(int i=0; i<n; ++i) ds.pop_back();
+    }
+    
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(!root) return {};
+        
+        vector<string> ans;
+        string ds;
+        
+        dfs(root, ds, ans);
+        
+        return ans;
+    }
+};
+
 
 // This solution handles when we need to return vector<vector<int>>
 class Solution {
@@ -43,3 +91,5 @@ public:
         return str;
     }
 };
+
+
