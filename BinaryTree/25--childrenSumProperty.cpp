6b18f2552901e058e7156e5cd1471730ddeef26b
coderajdeep <coder.rajdeep@gmail.com>
2022-06-30 20:48:54 +0530

    Added children sum property

--- BinaryTree/25--childrenSumProperty.cpp
+++ BinaryTree/25--childrenSumProperty.cpp
@@ -2,6 +2,46 @@
 // https://www.geeksforgeeks.org/check-for-children-sum-property-in-a-binary-tree/
 
 
+// recursive approach
+
+class Solution{
+    public:
+    int isSumProperty(Node *root)
+    {
+        if(!root) return 1;
+
+        if(!root->left && !root->right) return 1;
+
+        int sum = 0;
+        if(root->left) sum += root->left->data;
+        if(root->right) sum += root->right->data;
+        
+        return ((root->data==sum && isSumProperty(root->left) && isSumProperty(root->right)) ? 1 : 0);
+    }
+};
+
+
+// recursive approach
+
+class Solution{
+    public:
+    int isSumProperty(Node *root)
+    {
+        if(!root) return 1;
+
+        if(!root->left && !root->right) return 1;
+
+        int sum = 0;
+        if(root->left) sum += root->left->data;
+        if(root->right) sum += root->right->data;
+
+        if(root->data!=sum) return 0;
+
+        return ((isSumProperty(root->left) && isSumProperty(root->right)) ? 1 : 0);
+    }
+};
+
+
 // recursive solution
 
 int isSumProperty(Node *root) {
