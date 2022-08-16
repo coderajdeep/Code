90bd45a816a939dd4e54166224df9bbedf88b56a
coderajdeep <coder.rajdeep@gmail.com>
2022-08-16 13:03:00 +0530

    Update unique path - leetcode 61

--- DP/striver/8--uniquePaths--62.cpp
+++ DP/striver/8--uniquePaths--62.cpp
@@ -10,14 +10,14 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         if(m==1 || n==1) return 1;
-        vector<int>dp(n, 1);
-        vector<int>curr(n);
+        int dp[n];
+        int curr[n];
+        for(int i=0; i<n; ++i) dp[i] = 1;
         curr[0] = 1;
         for(int i=1; i<m; ++i) {
             for(int j=1; j<n; ++j) {
                 curr[j] = curr[j-1] + dp[j];
             }
-            // dp = curr;
             for(int i=0; i<n; ++i) dp[i] = curr[i];
         }
         return dp[n-1];
