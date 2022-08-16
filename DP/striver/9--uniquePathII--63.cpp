8e0a1dc5caced665855d35e493ea3ea6f2173e7a
coderajdeep <coder.rajdeep@gmail.com>
2022-08-16 21:47:06 +0530

    Space optimized - leetcode 63 - unique paths II

--- DP/striver/9--uniquePathII--63.cpp
+++ DP/striver/9--uniquePathII--63.cpp
@@ -1,6 +1,33 @@
 // leetcode 63
 // Unique paths II
 
+// Space optimized Tabulation - Bottom up approach
+// Time O(n*m)
+// Space O(m)
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        int prev[m];
+        int curr[m];
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<m; ++j) {
+                if(obstacleGrid[i][j]==1) curr[j] = 0;
+                else if(i==0 && j==0) curr[j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up + left;
+                }
+            }
+            for(int k=0; k<m; ++k) prev[k] = curr[k];
+        }
+        return curr[m-1];
+    }
+};
+
 // Tabulation - Bottom up approach
 // Time O(n*m)
 // Space O(n*m)
