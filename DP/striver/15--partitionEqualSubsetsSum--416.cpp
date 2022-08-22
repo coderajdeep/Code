01d40e329968e28a1d66a21eaa93e68fef3a3fcd
coderajdeep <coder.rajdeep@gmail.com>
2022-08-22 18:14:58 +0530

    partition equals subset sum - leetcode 416

--- DP/striver/15--partitionEqualSubsetsSum--416.cpp
+++ DP/striver/15--partitionEqualSubsetsSum--416.cpp
@@ -1,6 +1,31 @@
 // Leetcode 416
 // Partition equals subset sum
 
+
+// Tabulation
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; ++i) sum += nums[i];
+        if(sum%2==1) return false;
+        sum/=2;
+        bool dp[n][sum+1];
+        for(int i=1; i<=sum; ++i) dp[0][i] = (nums[0]==i);
+        for(int i=0; i<n; ++i) dp[i][0] = true;
+        for(int i=1; i<n; ++i) {
+            for(int j=1; j<=sum; ++j) {
+                bool pick = false;
+                if(j>=nums[i]) pick = dp[i-1][j-nums[i]];
+                bool notPick = dp[i-1][j];
+                dp[i][j] = pick || notPick;
+            }
+        }
+        return dp[n-1][sum];
+    }
+};
+
 // Memoization
 class Solution {
 public:
