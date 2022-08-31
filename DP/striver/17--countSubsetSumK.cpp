52570faacebae1fd3bdcbd9a6ba9eda8e7844e83
coderajdeep <coder.rajdeep@gmail.com>
2022-08-31 23:56:02 +0530

    Count subsets sum equals K - Space optimized tabulation

--- DP/striver/17--countSubsetSumK.cpp
+++ DP/striver/17--countSubsetSumK.cpp
@@ -1,24 +1,25 @@
 // Count subsets sum equals to K
 // https://www.codingninjas.com/codestudio/problems/number-of-subsets_3952532
 
-// Memoization
-int solve(int index, int tar, vector<int> &nums, vector<vector<int>> &dp) {
-    if(tar==0) return 1;
-    if(index==0) return (nums[0]==tar)?1:0;
-    if(dp[index][tar]!=-1) return dp[index][tar];
-    int take = 0;
-    if(nums[index]<=tar) take = solve(index-1, tar-nums[index], nums, dp);
-    int notTake = solve(index-1, tar, nums, dp);
-    return dp[index][tar] = take + notTake;
-}
 
+// Space optimized tabulation
 int findWays(vector<int> &num, int tar) {
     int n = num.size();
-    vector<vector<int>> dp(n, vector<int>(tar+1, -1));
-    int ans = solve(n-1, tar, num, dp);
-    return ans;
+    int prev[tar+1];
+    int curr[tar+1];
+    prev[0] = 1;
+    for(int i=1; i<=tar; ++i) prev[i] = (i==num[0])?1:0;
+    for(int i=1; i<n; ++i) {
+        for(int j=1; j<=tar; ++j) {
+            int take = 0;
+            if(num[i]<=j) take = prev[j-num[i]];
+            int notTake = prev[j];
+            curr[j] = take + notTake;
+        }
+        for(int k=1; k<=tar; ++k) prev[k] = curr[k];
+    }
+    return prev[tar];
 }
-
 
 // Tabulation
 int findWays(vector<int> &num, int tar) {
@@ -36,4 +37,22 @@ int findWays(vector<int> &num, int tar) {
         }
     }
     return dp[n-1][tar];
+}
+
+// Memoization
+int solve(int index, int tar, vector<int> &nums, vector<vector<int>> &dp) {
+    if(tar==0) return 1;
+    if(index==0) return (nums[0]==tar)?1:0;
+    if(dp[index][tar]!=-1) return dp[index][tar];
+    int take = 0;
+    if(nums[index]<=tar) take = solve(index-1, tar-nums[index], nums, dp);
+    int notTake = solve(index-1, tar, nums, dp);
+    return dp[index][tar] = take + notTake;
+}
+
+int findWays(vector<int> &num, int tar) {
+    int n = num.size();
+    vector<vector<int>> dp(n, vector<int>(tar+1, -1));
+    int ans = solve(n-1, tar, num, dp);
+    return ans;
 }
