2cee6b8b5411aed03a88cf52b5bf578ed7ed9245
coderajdeep <coder.rajdeep@gmail.com>
2022-09-04 14:55:25 +0530

    Tabulation - leetcode 494 - Target Sum

--- DP/striver/18--targetSum--494.cpp
+++ DP/striver/18--targetSum--494.cpp
@@ -2,7 +2,32 @@
 // Target Sum
 // Partitions With Given Difference
 
-// Memoization
+
+// Tabulation - Bottom up approach
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size(), sum = 0;
+        for(int i=0; i<n; ++i) sum += nums[i];
+        int tar = sum + target;
+        if(tar<0 || tar%2==1) return 0;
+        tar /= 2;
+        int dp[n][tar+1];
+        dp[0][0] = (nums[0]==0)?2:1;
+        for(int i=1; i<=tar; ++i) dp[0][i] = (nums[0]==i)?1:0;
+        for(int i=1; i<n; ++i) {
+            for(int j=0; j<=tar; ++j) {
+                int take = 0;
+                if(nums[i]<=j) take = dp[i-1][j-nums[i]];
+                int notTake = dp[i-1][j];
+                dp[i][j] = take + notTake;
+            }
+        }
+        return dp[n-1][tar];
+    }
+};
+
+// Memoization - Top Down approach
 class Solution {
 public:
     int solve(int index, int tar, vector<int> &nums, vector<vector<int>> &dp) {
