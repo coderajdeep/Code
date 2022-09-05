abf524b0bbb3d2196f31a4682af28601bf0ba2b8
coderajdeep <coder.rajdeep@gmail.com>
2022-09-06 00:00:51 +0530

    0 1 knapsack - tabulation

--- DP/striver/19--01knapsack.cpp
+++ DP/striver/19--01knapsack.cpp
@@ -1,6 +1,23 @@
 // Geeks
 // Coding Ninja
 
+
+// Bottom up approach - tabulation
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+    int dp[n][W+1];
+    for(int i=0; i<=W; ++i) dp[0][i] = (wt[0]<=i)?val[0]:0;
+    for(int i=1; i<n; ++i) {
+        for(int j=0; j<=W; ++j) {
+            int pick = 0;
+            if(wt[i]<=j) pick = val[i] + dp[i-1][j-wt[i]];
+            int notPick = dp[i-1][j];
+            dp[i][j] = max(pick, notPick);
+        }
+    }
+    return dp[n-1][W];
+}
+
 // Top down approach
 // Memoization
 int solve(int index, int W, int *wt, int *val, vector<vector<int>> &dp) {
