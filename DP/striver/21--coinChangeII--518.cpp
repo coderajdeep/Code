afe8e03d7811e3148347f669938d7f0bdf763395
coderajdeep <coder.rajdeep@gmail.com>
2022-09-11 15:18:38 +0530

    Coin Change II - leetcode 518 - Tabulation

--- DP/striver/21--coinChangeII--518.cpp
+++ DP/striver/21--coinChangeII--518.cpp
@@ -1,6 +1,25 @@
 // Coin Change II
 // Leetcode 518
 
+
+// Bottom up approach
+// Tabulation
+int change(int amount, vector<int>& coins) {
+    int n = coins.size();
+    int dp[n][amount+1];
+    for(int i=0; i<=amount; ++i) dp[0][i] = (i%coins[0]==0)?1:0;
+    for(int i=1; i<n; ++i) {
+        for(int j=0; j<=amount; ++j) {
+            int take = 0;
+            if(coins[i]<=j) take = dp[i][j-coins[i]];
+            int notTake = dp[i-1][j];
+            dp[i][j] = take + notTake;
+        }
+    }
+    return dp[n-1][amount];
+}
+
+
 // Top Down Approach
 // Memoization
 int solve(int index, int tar, vector<int> &coins, vector<vector<int>> &dp) {
