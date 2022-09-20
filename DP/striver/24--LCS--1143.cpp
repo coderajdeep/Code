fd7c6b4c1432d8a786f8b6c69e44789dc7e2a5ed
coderajdeep <coder.rajdeep@gmail.com>
2022-09-20 20:02:06 +0530

    Longest common substring - leetcode 1143 - space optimized

--- DP/striver/24--LCS--1143.cpp
+++ DP/striver/24--LCS--1143.cpp
@@ -2,6 +2,28 @@
 // leetcode 1143
 
 
+// Space optimized
+// tabulation
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        int dp[2][m+1];
+        for(int i=0; i<=m; ++i) dp[0][i] = 0;
+        for(int i=1; i<=n; ++i) {
+            dp[1][0] = 0;
+            for(int j=1; j<=m; ++j) {
+                if(text1[i-1]==text2[j-1]) dp[1][j] = 1 + dp[0][j-1];
+                else dp[1][j] = max(dp[1][j-1], dp[0][j]);
+            }
+            for(int i=1; i<=m; ++i) dp[0][i] = dp[1][i];
+        }
+        return dp[0][m];
+    }
+};
+
+
 // Bottom up approach
 // tabulation
 class Solution {
