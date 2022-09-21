199476efa993e7c58f8064d55c5443d7d0c4f77c
coderajdeep <coder.rajdeep@gmail.com>
2022-09-22 00:19:40 +0530

    Delete Operation for Two Strings

--- DP/dp/2--minDeleteOperationString--583.cpp
+++ DP/dp/2--minDeleteOperationString--583.cpp
@@ -8,8 +8,8 @@ public:
         int m = word2.size();
         int dp[2][m+1];
         for(int i=0; i<=m; ++i) dp[0][i] = 0;
+        dp[1][0] = 0;
         for(int i=1; i<=n; ++i) {
-            dp[1][0] = 0;
             for(int j=1; j<=m; ++j) {
                 if(word1[i-1]==word2[j-1]) dp[1][j] = 1 + dp[0][j-1];
                 else dp[1][j] = max(dp[1][j-1], dp[0][j]);
