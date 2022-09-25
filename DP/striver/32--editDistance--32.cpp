310722a398664bf3cb634036186aa6aaf190df31
coderajdeep <coder.rajdeep@gmail.com>
2022-09-25 18:03:01 +0530

    Edit distance - leetcode 32 - Memoization - Top down approach

---
+++ DP/striver/32--editDistance--32.cpp
@@ -0,0 +1,28 @@
+// leetcode 32
+// Edit distance
+
+// Top down approach
+// Mamoization
+class Solution {
+public:
+    int solve(int n, int m, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(n==0 && m==0) return 0;
+        if(n==0) return m;
+        if(m==0) return n;
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(s1[n-1]==s2[m-1]) return dp[n][m] = solve(n-1, m-1, s1, s2, dp);
+        else {
+            int rep = solve(n-1, m-1, s1, s2, dp);
+            int del = solve(n-1, m, s1, s2, dp);
+            int in = solve(n, m-1, s1, s2, dp);
+            return dp[n][m] = 1 + min({rep, del, in});
+        }
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        int ans = solve(n, m, word1, word2, dp);
+        return ans;
+    }
+};
