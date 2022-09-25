2ffe2c8cda1a0fb8724858b08d844723e8fb0997
coderajdeep <coder.rajdeep@gmail.com>
2022-09-25 13:47:47 +0530

    Distinct subsequence - Bottom up approach - leetcode 115

--- DP/striver/31--distinctSubsequence--115.cpp
+++ DP/striver/31--distinctSubsequence--115.cpp
@@ -1,6 +1,33 @@
 // leetcode 115
 // Distinct Subsequences
 
+
+// Bottom up approach
+// Tabulation
+/*
+Below test case was failing while taking int dp[n+1][m+1]
+"xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsuljkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglewkycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdejletqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsggleiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghnatexyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboyzaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdzqhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykgscxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjjnygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizbadynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpgtgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwyiglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowblqvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonysahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmhbdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlcntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyfdejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgyrqmjaeepydrcchfyftjighntqzoo"
+"rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys"
+*/
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        unsigned int dp[n+1][m+1];
+        for(int i=1; i<=m; ++i) dp[0][i] = 0;
+        dp[0][0] = 1;
+        for(int i=1; i<=n; ++i) {
+            dp[i][0] = 1;
+            for(int j=1; j<=m; ++j) {
+                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
+};
+
 // Top down approach
 // Memoization
 class Solution {
