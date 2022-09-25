9d571404e18986fddaa2319698ba47e5b21c2a67
coderajdeep <coder.rajdeep@gmail.com>
2022-09-25 18:28:01 +0530

    Edit distance - leetcode 32 - Tabulation - Bottom up approach

--- DP/striver/32--editDistance--32.cpp
+++ DP/striver/32--editDistance--32.cpp
@@ -1,6 +1,27 @@
 // leetcode 72
 // Edit distance
 
+// Bottom up approach
+// Tabulation
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        int dp[n+1][m+1];
+        for(int i=0; i<=m; ++i) dp[0][i] = i;
+        for(int i=0; i<=n; ++i) dp[i][0] = i;
+        for(int i=1; i<=n; ++i) {
+            for(int j=1; j<=m; ++j) {
+                if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+                // replace : dp[i-1][j-1]; insert : dp[i][j-1]; delete : dp[i-1][j]
+            }
+        }
+        return dp[n][m];
+    }
+};
+
 // Top down approach
 // Mamoization
 class Solution {
