a78a83b466046dc0ab43c353214b3f5f0c4b33f9
coderajdeep <coder.rajdeep@gmail.com>
2022-11-20 10:14:06 +0530

    No of provinces - geeksforgeeks

--- Graph/10--noOfProvinces--547.cpp
+++ Graph/10--noOfProvinces--547.cpp
@@ -55,3 +55,43 @@ int findCircleNum(vector<vector<int>>& isConnected) {
     }
     return count;
 }
+
+
+// Geeksforgeeks
+class Solution {
+  public:
+    void bfs(int source, int V, vector<vector<int>> &adj, vector<bool> &visited) {
+        queue<int>q;
+        q.push(source);
+        visited[source] = true;
+        while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for(int i=0; i<V; ++i) {
+                if(adj[u][i]==1 && !visited[i]) {
+                    q.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+    void dfs(int source, int V, vector<vector<int>> &adj, vector<bool> &visited) {
+        visited[source] = true;
+        for(int i=0; i<V; ++i) {
+            if(adj[source][i]==1 && !visited[i]) {
+                dfs(i, V, adj, visited);
+            }
+        }
+    }
+    int numProvinces(vector<vector<int>> adj, int V) {
+        vector<bool> visited(V, false);
+        int count = 0;
+        for(int i=0; i<V; ++i) {
+            if(!visited[i]) {
+                ++count;
+                dfs(i, V, adj, visited);
+            }
+        }
+        return count;
+    }
+};
