ee70862968c17f86d20a816e137a030031b4e3a9
coderajdeep <coder.rajdeep@gmail.com>
2023-08-27 16:11:21 +0530

    Update array inversion count

--- striverSDE/array/12--arrayInversionCount.cpp
+++ striverSDE/array/12--arrayInversionCount.cpp
@@ -15,8 +15,7 @@ long long getInversions(long long *arr, int n){
 
 // Time complexity O(nlogn)
 // Space complexity O(n)
-int merge(int low, int mid, int high, long long *arr, long long *temp) {
-    int cnt = 0;
+long long merge(int low, int mid, int high, long long *arr, long long *temp) {
     for(int i=low; i<=mid; ++i) {
         temp[i] = arr[i];
     }
@@ -24,34 +23,36 @@ int merge(int low, int mid, int high, long long *arr, long long *temp) {
         temp[i] = arr[i];
     }
     int index = low, i = low, j = mid+1;
+    long long count = 0;
     while(i<=mid && j<=high) {
         if(temp[i]<=temp[j]) {
             arr[index++] = temp[i++];
         }
         else {
-            cnt += (mid - i + 1);
+            count += (mid - i + 1);
             arr[index++] = temp[j++];
         }
     }
     while(i<=mid) {
         arr[index++] = temp[i++];
     }
-    while(j<=mid) {
+    while(j<=high) {
         arr[index++] = temp[j++];
     }
-    return cnt;
+    return count;
 }
-int mergeSort(int low, int high, long long *arr, long long *temp) {
-    int cnt = 0;
-    if(low>=high) return cnt;
+long long mergeSort(int low, int high, long long *arr, long long *temp) {
+    if(low>=high) return 0;
+    long long count = 0;
     int mid = (high-low)/2 + low;
-    cnt += mergeSort(low, mid, arr, temp);
-    cnt += mergeSort(mid+1, high, arr, temp);
-    cnt += merge(low, mid, high, arr, temp);
-    return cnt;
+    count += mergeSort(low, mid, arr, temp);
+    count += mergeSort(mid+1, high, arr, temp);
+    count += merge(low, mid, high, arr, temp);
+    return count;
 }
 long long getInversions(long long *arr, int n){
     long long *temp = new long long[n];
-    int ans = mergeSort(0, n-1, arr, temp);
+    long long ans = mergeSort(0, n-1, arr, temp);
+    delete[] temp;
     return ans;
 }
