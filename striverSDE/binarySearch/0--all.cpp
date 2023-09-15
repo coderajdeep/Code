aaa139660afdbe011f0a812f63168ae6606b418b
coderajdeep <coder.rajdeep@gmail.com>
2023-09-15 21:31:22 +0530

    Implement lower bound

--- striverSDE/binarySearch/0--all.cpp
+++ striverSDE/binarySearch/0--all.cpp
@@ -1,2 +1,5 @@
 // 1
 // Binary serach on array - leetcode 704
+
+// 2
+// Implement lower bound for non decreasing array - coding ninja
---
+++ striverSDE/binarySearch/2--lowerBound.cpp
@@ -0,0 +1,26 @@
+// Implement lower bound for non decreasing array - coding ninja
+// https://www.codingninjas.com/studio/problems/lower-bound_8165382
+
+// Time complexity O(logn)
+// Space complexity O(1)
+int lowerBound(vector<int> arr, int n, int x) {
+	int low = 0, high = n-1, mid;
+	if(x<arr[0]) {
+		low = 0;
+	}
+	else if(x>arr[n-1]) {
+		low = n;
+	}
+	else {
+		while(low<high) {
+			mid = low + (high - low) / 2;
+			if(x <= arr[mid]) {
+				high = mid;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+	}
+	return low;
+}
