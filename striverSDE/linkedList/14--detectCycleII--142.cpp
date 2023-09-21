c2bc55c9c75371b96fccd7caf9e2972149bd487a
coderajdeep <coder.rajdeep@gmail.com>
2023-09-21 21:12:05 +0530

    Detect cycle II

--- striverSDE/linkedList/14--detectCycleII--142.cpp
+++ striverSDE/linkedList/14--detectCycleII--142.cpp
@@ -1,5 +1,8 @@
 // Find where the loop begin in linkedlist -- leetcode 142
 
+// Time complexity O(n)
+// Space complexity O(n)
+// Using unordered_set
 ListNode *detectCycle(ListNode *head) {
     unordered_set<ListNode*>hashNode;
     while(head) {
@@ -10,4 +13,32 @@ ListNode *detectCycle(ListNode *head) {
         head = head->next;
     }
     return nullptr;
+}
+
+// Time complexity O(n)
+// Space complexity O(1)
+// Slow and fast pointer approach
+ListNode *detectCycle(ListNode *head) {
+    if(!head || !head->next) {
+        return nullptr;
+    }
+    ListNode *slow = head, *fast = head;
+    bool isLoopExist = false;
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(fast==slow) {
+            isLoopExist = true;
+            break;
+        }
+    }
+    if(!isLoopExist) {
+        return nullptr;
+    }
+    slow = head;
+    while(slow!=fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow;
 }
