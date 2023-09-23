9215bb0ebbcad4dc6287df398d6d0ae6c4a8082b
coderajdeep <coder.rajdeep@gmail.com>
2023-09-23 14:06:42 +0530

    Delete n-th node from end

--- striverSDE/linkedList/18--removeNthNodeFromEnd--19.cpp
+++ striverSDE/linkedList/18--removeNthNodeFromEnd--19.cpp
@@ -1,5 +1,48 @@
 // Remove N-th node from end of the linkedlist -- leetcode 19
 
+// Time complexity O(n)
+// Space complexity O(1)
+// Single traversal
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    // this is for when n==1 and head==nullptr
+    if(!head) {
+        return head;
+    }
+    ListNode *ptr = head, *curr = head;
+    for(int i=1; i<n; ++i) {
+        // when n is greater than the length of linkedlist
+        if(!ptr) {
+            return nullptr;
+        }
+        ptr = ptr->next;
+    }
+    ListNode *temp = nullptr;
+    // When n == size of linkedlist
+    // So need to delete the first node
+    if(!ptr->next) {
+        temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+
+    ListNode *prev = nullptr;
+    while(ptr && ptr->next) {
+        prev = curr;
+        curr = curr->next;
+        ptr = ptr->next;
+    }
+    // we need to delete curr node
+    // prev is the previous of curr node
+    prev->next = curr->next;
+    delete curr;
+    return head;
+}
+
+
+// Time complexity O(n)
+// Space complexity O(1)
+// Single traversal
 ListNode* removeNthFromEnd(ListNode* head, int n) {
     // this is for when n==1 and head==nullptr
     if(!head) {
