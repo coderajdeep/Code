85cb9aa2b22c50b27bbdaf800aee1f8d8400bbdd
coderajdeep <coder.rajdeep@gmail.com>
2023-10-29 13:23:02 +0530

    Construct tree from array

--- striverSDE/binaryTree/0--all.cpp
+++ striverSDE/binaryTree/0--all.cpp
@@ -1,2 +1,5 @@
 // 1
 // Number of nodes in N-th level
+
+// 2
+// Construct tree from a array [Array length is seven and elements are in level order]
---
+++ striverSDE/binaryTree/2--constructTreeFromArray.cpp
@@ -0,0 +1,12 @@
+// Construct tree from a array [Array length is seven and elements are in level order]
+
+Node* createTree(vector<int>&arr){
+    Node *root = new Node(arr[0]);
+    root->left = new Node(arr[1]);
+    root->right = new Node(arr[2]);
+    root->left->left = new Node(arr[3]);
+    root->left->right = new Node(arr[4]);
+    root->right->left = new Node(arr[5]);
+    root->right->right = new Node(arr[6]);
+    return root;
+}
