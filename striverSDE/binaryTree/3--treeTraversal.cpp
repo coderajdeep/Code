fc6d7ca0a1c14e4426e29980b36042ec71f2d955
coderajdeep <coder.rajdeep@gmail.com>
2023-11-06 19:10:52 +0530

    DFS traversal (inorder, preorder and postorder) traversal of binary tree

---
+++ striverSDE/binaryTree/3--treeTraversal.cpp
@@ -0,0 +1,27 @@
+// Binary Tree traversal
+
+void inOrder(TreeNode* root, vector<int>& inorder) {
+    if(!root) return;
+    inOrder(root->left, inorder);
+    inorder.push_back(root->data);
+    inOrder(root->right, inorder);
+}
+void preOrder(TreeNode* root, vector<int>& preorder) {
+    if(!root) return;
+    preorder.push_back(root->data);
+    preOrder(root->left, preorder);
+    preOrder(root->right, preorder);
+}
+void postOrder(TreeNode* root, vector<int>& postorder) {
+    if(!root) return;
+    postOrder(root->left, postorder);
+    postOrder(root->right, postorder);
+    postorder.push_back(root->data);
+}
+vector<vector<int>> getTreeTraversal(TreeNode *root){
+    vector<int> preorder, inorder, postorder;
+    inOrder(root, inorder);
+    preOrder(root, preorder);
+    postOrder(root, postorder);
+    return vector<vector<int>> {inorder, preorder, postorder};
+}
