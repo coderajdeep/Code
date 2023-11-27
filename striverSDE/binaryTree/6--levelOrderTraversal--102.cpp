7934b25c61422b7b631dff743b757fceb16b969f
coderajdeep <coder.rajdeep@gmail.com>
2023-11-27 19:20:27 +0530

    Level order traversal by printing each level in a recursive function

--- striverSDE/binaryTree/6--levelOrderTraversal--102.cpp
+++ striverSDE/binaryTree/6--levelOrderTraversal--102.cpp
@@ -55,3 +55,30 @@ vector<int> levelOrder(TreeNode<int> * root){
     }
     return levelOrder;
 }
+
+// Time complexity O(n2)
+// Space complexity O(n)
+void printNthLevel(TreeNode* root, int level, vector<int> &levelOrder) {
+    if(!root) return;
+    if(level==1) {
+        levelOrder.push_back(root->val);
+    }
+    else if(level > 1) {
+        printNthLevel(root->left, level-1, levelOrder);
+        printNthLevel(root->right, level-1, levelOrder);
+    }
+}
+int height(TreeNode* root) {
+    return !root ? 0 : (max(height(root->left), height(root->right)) + 1);
+}
+vector<vector<int>> levelOrder(TreeNode* root) {
+    if(!root) return vector<vector<int>> {};
+    vector<vector<int>> levelOrderTraversal;
+    int ht = height(root);
+    for(int i=1; i<=ht; ++i) {
+        vector<int>levelOrder;
+        printNthLevel(root, i, levelOrder);
+        levelOrderTraversal.push_back(levelOrder);
+    }
+    return levelOrderTraversal;
+}
