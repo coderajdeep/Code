9b09ef2c633e9d6f95b5e3f3328f8f65fc1ed69c
coderajdeep <coder.rajdeep@gmail.com>
2023-11-27 14:15:21 +0530

    Iterative inorder traversal

---
+++ striverSDE/binaryTree/5--inorderTraversal--94.cpp
@@ -0,0 +1,22 @@
+// leetcode Iterative Inorder Traversal
+vector<int> inorderTraversal(TreeNode* root) {
+    if(!root) return vector<int> {};
+
+    vector<int> inorder;
+    TreeNode* curr = root;
+    stack<TreeNode*> stk;
+
+    while(!stk.empty() || curr) {
+        if(curr) {
+            stk.push(curr);
+            curr = curr->left;
+        }
+        else {
+            curr = stk.top();
+            stk.pop();
+            inorder.push_back(curr->val);
+            curr = curr->right;
+        }
+    }
+    return inorder;
+}
