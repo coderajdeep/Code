cde8e4f20141490238705192fecef854b0c046b8
coderajdeep <coder.rajdeep@gmail.com>
2023-12-08 19:27:49 +0530

    Remove duplicate element from sorted array

--- striverSDE/arrayUpdated/0--all.cpp
+++ striverSDE/arrayUpdated/0--all.cpp
@@ -1,5 +1,5 @@
 // 1
-// Largest element in ana array
+// Largest element in an array
 
 // 2
 // Find 2nd largest and 2nd smallest element
@@ -8,4 +8,4 @@
 // Check if and array is sorted or not - coding ninjas (We can't check if an array is sorted or not in logN complexity)
 
 // 4
-// Remove duplicate from sorted array
+// Remove duplicate from sorted array -- leetcode 26
