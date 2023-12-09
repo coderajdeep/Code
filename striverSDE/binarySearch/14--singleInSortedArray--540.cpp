d5f005de257a1020e4d156a2a7aa0eec88bde097
coderajdeep <coder.rajdeep@gmail.com>
2023-12-09 21:03:45 +0530

    Single element in sorted array

--- striverSDE/binarySearch/14--singleInSortedArray--540.cpp
+++ striverSDE/binarySearch/14--singleInSortedArray--540.cpp
@@ -1,6 +1,8 @@
 // Single element in sorted array - leetcode 540
 
+// Best approach
 // Time complexity O(logn)
+// Space complexity O(1)
 int singleNonDuplicate(vector<int>& nums) {
     int n = nums.size();
     if(n==1) return nums[0];
@@ -31,4 +33,41 @@ int singleNonDuplicate(vector<int>& nums) {
         }
     }
     return -1;
+}
+
+// Coding Ninjas
+int getSingleElement(vector<int> &arr){
+	int n = arr.size();
+	if(n==1) return arr[0];
+	int lo = 0, hi = n-1, mid;
+	while(lo<=hi) {
+		mid = lo + (hi-lo)/2;
+		if(mid-1>=0 && mid+1<n && arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]) {
+			return arr[mid];
+		}
+		else if(mid-1<0 && arr[mid]!=arr[mid+1]) {
+			return arr[mid];
+		}
+		else if(mid+1>=n && arr[mid]!=arr[mid-1]) {
+			return arr[mid];
+		}
+		// odd
+		if(mid&1) {
+			if(arr[mid]==arr[mid+1]) {
+				hi = mid-1;
+			}
+			else {
+				lo = mid+1;
+			}
+		}
+		else {
+			if(arr[mid]==arr[mid+1]) {
+				lo = mid+1;
+			}
+			else {
+				hi = mid-1;
+			}
+		}
+	}
+	return -1;
 }
