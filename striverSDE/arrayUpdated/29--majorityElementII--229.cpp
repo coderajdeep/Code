aa15509db75cec94b02393f633fb6639c002a129
coderajdeep <coder.rajdeep@gmail.com>
2024-02-01 22:51:42 +0530

    Majority element II

---
+++ striverSDE/arrayUpdated/29--majorityElementII--229.cpp
@@ -0,0 +1,53 @@
+// Majority element II
+
+// Time complexity O(n)
+// Space complexity O(n)
+vector<int> majorityElement(vector<int>& nums) {
+    unordered_multiset<int>ums;
+    vector<int> ans;
+    int len = nums.size()/3;
+    for(int element:nums) {
+        ums.insert(element);
+        if(ums.count(element)>len) {
+            if(ans.empty() || ans.back()!=element) {
+                ans.push_back(element);
+            }
+            if(ans.size()==2) return ans;
+        }
+    }
+    return ans;
+}
+
+// Time complexity O(n)
+// Space complexity O(1)
+vector<int> majorityElement(vector<int>& nums) {
+    int count1 = 0, count2 = 0, element1 = INT_MIN, element2 = INT_MIN, len = nums.size()/3;
+    for(int element:nums) {
+        if(element1 == element) ++count1;
+        else if(element2 == element) ++count2;
+        else if(count1 == 0) {
+            element1 = element;
+            count1 = 1;
+        }
+        else if(count2 == 0) {
+            element2 = element;
+            count2 = 1;
+        }
+        else {
+            --count1;
+            --count2;
+        }
+    }
+    return majorityCheck(element1, element2, len, nums);
+}
+vector<int> majorityCheck(int element1, int element2, int len, vector<int> &arr) {
+    int cnt1 = 0, cnt2 = 0;
+    vector<int> ans;
+    for(int element:arr) {
+        if(element==element1) ++cnt1;
+        if(element==element2) ++cnt2;
+    }
+    if(cnt1>len) ans.push_back(element1);
+    if(cnt2>len) ans.push_back(element2);
+    return ans;
+}
