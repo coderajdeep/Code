64a034a1baaa17bcdc008470931f9d91b04e75ef
coderajdeep <coder.rajdeep@gmail.com>
2024-02-29 00:17:22 +0530

    Min stack

--- striverSDE/stackQueue/8--minStack--155.cpp
+++ striverSDE/stackQueue/8--minStack--155.cpp
@@ -1,5 +1,69 @@
 // Implement Minimum stack -- leetcode 155
 
+
+// Time complexity O(1)
+// Space complexity O(n)
+// Best approach
+class MinStack {
+public:
+    long minElement, topElement;
+    stack<long> stk;
+    MinStack() {
+        // clearing stack
+        while(!stk.empty()) {
+            stk.pop();
+        }
+    }
+    
+    void push(int val) {
+        if(stk.empty()) {
+            minElement = val;
+            stk.push(val);
+        }
+        else {
+            if(val < minElement) {
+                stk.push(2 * (long)val - minElement);
+                minElement = val;
+            }
+            else {
+                stk.push(val);
+            }
+        }
+    }
+    /*
+    // this will also work
+    void push(int val) {
+        if(val < minElement) {
+            stk.push(2*(long)val - minElement);
+        }
+        else {
+            stk.push(val);
+        }
+        minElement = min(minElement, val);
+    }
+    */
+    
+    void pop() {
+        if(stk.empty()) return;
+        topElement = stk.top();
+        if(topElement < minElement) {
+            minElement = 2 * (long)minElement - topElement;
+        }
+        stk.pop();
+    }
+    
+    int top() {
+        if(stk.empty()) return -1;
+        topElement = stk.top();
+        if(topElement < minElement) return minElement;
+        else return topElement;
+    }
+    
+    int getMin() {
+        return minElement;
+    }
+};
+
 // Time complexity O(1)
 // Space complexity O(2*n)
 class MinStack {
@@ -49,65 +113,5 @@ public:
             p = stk.top();
             return p.second;
         }
-    }
-};
-
-// Time complexity O(1)
-// Space complexity O(n)
-class MinStack {
-private:
-    stack<long> stk;
-    long minElement, topElement;
-    void clearStack() {
-        while(!stk.empty()) {
-            stk.pop();
-        }
-    }
-public:
-    MinStack() {
-        clearStack();
-        minElement = INT_MAX;
-    }
-    
-    void push(int val) {
-        if(minElement == LONG_MAX) {
-            minElement = LONG_MAX;
-            stk.push(val);
-        }
-        else if(val < minElement) {
-            // ((2 * val) - minElement) < minElement
-            stk.push((long)2 * val - minElement);
-            minElement = val;
-        }
-        else {
-            stk.push(val);
-        }
-    }
-    
-    void pop() {
-        if(stk.empty()) {
-            return;
-        }
-        topElement = stk.top();
-        cout<<topElement<<endl;
-        if(topElement < minElement) {
-            minElement = ((long)2 * minElement - topElement);
-        }
-        stk.pop();
-    }
-    
-    int top() {
-        if(stk.empty()) {
-            return -1;
-        }
-        topElement = stk.top();
-        if(topElement < minElement) {
-            return minElement;
-        }
-        return topElement;
-    }
-    
-    int getMin() {
-        return minElement;
     }
 };
