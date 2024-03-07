953867d134c91f01e48d5cd62a96d609b9833c4f
coderajdeep <coder.rajdeep@gmail.com>
2024-03-07 23:22:26 +0530

    Update base case in coin change

--- striverSDE/dp/08--uniquePath--62.cpp
+++ striverSDE/dp/08--uniquePath--62.cpp
@@ -5,14 +5,15 @@
 // Time complexity O(n*m)
 // Space complexity O(n*m) + Recursion Stack Space O(n*m)
 int uniquePathUtils(int r, int c, vector<vector<int>> &dp) {
-    if(r==0 || c==0) return 1;
+    if(r==0 && c==0) return 1;
+    if(r<0 || c<0) return 0;
     if(dp[r][c] != -1) return dp[r][c];
     int left = uniquePathUtils(r, c-1, dp);
     int up = uniquePathUtils(r-1, c, dp);
     return dp[r][c] = (left + up);
 }
 int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m, vector<int>(n, -1));
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
     return uniquePathUtils(m-1, n-1, dp);
 }
 
@@ -21,12 +22,13 @@ int uniquePaths(int m, int n) {
 // Time complexity O(n*m)
 // Space complexity O(n*m)
 int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m, vector<int>(n, 1));
-    for(int i=1; i<m; ++i) {
-        for(int j=1; j<n; ++j) {
+    vector<vector<int>> dp(m, vector<int>(n));
+    for(int i=0; i<m; ++i) {
+        for(int j=0; j<n; ++j) {
             // left -> dp[i][j-1]
             // up -> dp[i-1][j]
-            dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            if(i==0 || j==0) dp[i][j] = 1;
+            else dp[i][j] = dp[i][j-1] + dp[i-1][j];
         }
     }
     return dp[m-1][n-1];
