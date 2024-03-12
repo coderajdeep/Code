08cf2e36e49ff56229844104992f19497c907d05
coderajdeep <coder.rajdeep@gmail.com>
2024-03-12 08:42:34 +0530

    Number of subsets with given sum, when array elements are 0 <= arr[i] <= 1000

---
+++ HLD/networks.md
@@ -0,0 +1,6 @@
+# What is network protocal ?
+* A network protocol is a set of rules that defines data communication between different devices over the network. Different divices like computers, servers, routers, virtual machines have different underlying architecture, designs or standards. They follows the network protocal to communicate with each other.
+
+# OSI Model
+
+# Application layer and Transport Layer
--- striverSDE/dp/17--countSubsetWithSumK.cpp
+++ striverSDE/dp/17--countSubsetWithSumK.cpp
@@ -4,11 +4,16 @@
 // Memoization
 // Time complexity O(n*k)
 // Space complexity O(n*k) + Recursion stack space O(n*k)
+// https://youtu.be/zoilQD1kYSg
+// How to handle when // 0 <= arr[i] <=1000
 const int mod = 1e9 + 7;
 int getTotalSubsetSum(int index, int k, vector<int> &nums, vector<vector<int>> &dp) {
-    if(k==0) return 1;
     if(k<0) return 0;
-    if(index<0) return 0;
+    if(index==0) {
+        if(k==0 && nums[0]==0) return 2;
+        else if(k==0 || nums[0]==k) return 1;
+        else return 0;
+    }
     if(dp[index][k] != -1) return dp[index][k];
     int pick = getTotalSubsetSum(index-1, k-nums[index], nums, dp) % mod;
     int notPick = getTotalSubsetSum(index-1, k, nums, dp) % mod;
@@ -26,15 +31,18 @@ int findWays(vector<int>& arr, int k)
 // Tabulation
 // Time complexity O(n*k)
 // Space complexity O(n*k)
+// 0 <= arr[i] <=1000
 int findWays(vector<int>& arr, int k)
 {
     int n = arr.size(), mod = 1e9+7;
     vector<vector<int>> dp(n, vector<int>(k+1, 0));
-    for(int i=0; i<n; ++i) dp[i][0] = 1;
-    if(arr[0] <= k) dp[0][arr[0]] = 1;
+    
+    dp[0][0] = 1;
+    if(arr[0]==0) dp[0][0] = 2;
+    else if(arr[0] <= k) dp[0][arr[0]] = 1;
 
     for(int i=1; i<n; ++i) {
-        for(int j=1; j<=k; ++j) {
+        for(int j=0; j<=k; ++j) {
             int pick = (arr[i] <= j) ? dp[i-1][j-arr[i]] : 0;
             int notPick = dp[i-1][j];
             dp[i][j] = (pick + notPick) % mod;
@@ -46,19 +54,28 @@ int findWays(vector<int>& arr, int k)
 // Space optimised
 // Time complexity O(n*k)
 // Space complexity O(k)
-int findWays(vector<int>& arr, int k) {
+// 0 <= arr[i] <=1000
+int findWays(vector<int>& arr, int k)
+{
     int n = arr.size(), mod = 1e9+7;
     vector<int> prev(k+1, 0), curr(k+1);
-    prev[0] = curr[0] = 1;
-    if(arr[0] <= k) prev[arr[0]] = 1;
+    // By default if sum == 0 then prev[0] = 1
+    prev[0] = 1;
+    // If arr[0] == 0
+    // then we can include or exclude arr[0] into our subset
+    // that's why answer is 2
+    if(arr[0]==0) prev[0] = 2;
+    else {
+        if(arr[0] <= k) prev[arr[0]] = 1;
+    }
 
     for(int i=1; i<n; ++i) {
-        for(int j=1; j<=k; ++j) {
+        for(int j=0; j<=k; ++j) {
             int pick = (arr[i] <= j) ? prev[j-arr[i]] : 0;
             int notPick = prev[j];
             curr[j] = (pick + notPick) % mod;
         }
-        for(int j=1; j<=k; ++j) {
+        for(int j=0; j<=k; ++j) {
             prev[j] = curr[j];
         }
     }
