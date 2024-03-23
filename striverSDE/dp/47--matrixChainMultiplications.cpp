67d6a040341937b30268ac64c361e246cbdc6fbe
coderajdeep <coder.rajdeep@gmail.com>
2024-03-23 14:39:37 +0530

    Matrix chain multiplication

--- striverSDE/dp/47--matrixChainMultiplications.cpp
+++ striverSDE/dp/47--matrixChainMultiplications.cpp
@@ -1,4 +1,5 @@
 // Matrix Chain Multiplications -- Coding Ninjas
+// Memoization
 // Time complexity O(n*n*n)
 // Space complexity O(n*n) + O(n) Recursion stack space
 int getMinMultiplication(int i, int j, int *arr, vector<vector<int>> &dp) {
@@ -20,4 +21,26 @@ int getMinMultiplication(int i, int j, int *arr, vector<vector<int>> &dp) {
 int matrixChainMultiplication(int* arr, int n) {
     vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
     return getMinMultiplication(1, n, arr, dp);
+}
+
+
+// Tabulation
+// Time complexity O(n*n*n)
+// Space complexity O(n*n)
+int matrixChainMultiplication(int* arr, int n) {
+    int dp[n+1][n+1];
+    for(int i=n; i>=1; --i) {
+        dp[i][i] = 0;
+        for(int j=i+1; j<=n; ++j) {
+            int minOperation = INT_MAX;
+            for(int k=i; k<j; ++k) {
+                int firstPartition = dp[i][k];
+                int secondPartition = dp[k+1][j];
+                int totalOperation = arr[i-1]*arr[k]*arr[j] + firstPartition + secondPartition;
+                minOperation = min(minOperation, totalOperation);
+            }
+            dp[i][j] = minOperation;
+        }
+    }
+    return dp[1][n];
 }
