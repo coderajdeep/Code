72f5f83b97b631a1fc4667aaa7aa685999f8ef7e
coderajdeep <coder.rajdeep@gmail.com>
2024-04-03 23:47:57 +0530

    Binary string without consecutive 1

--- striverSDE/recursion/0--all.cpp
+++ striverSDE/recursion/0--all.cpp
@@ -6,3 +6,7 @@
 
 // 3
 // Count Good numbers -- leetcode 1922
+
+// 6
+// Gererate all binary string without consecutive 1's -- Geeks
+// If we need to find the count then its a imp DP question
---
+++ striverSDE/recursion/06--binaryStringWithoutConsecutive1's.cpp
@@ -0,0 +1,33 @@
+// Gererate all binary string without consecutive 1's -- Geeks
+// If we need to find the count then its a imp DP question
+// Time complexity O(2^n)
+// Space complexity O(2^n)
+void utils(int index, int N, string &s, vector<string> &ans) {
+    if(index==N) {
+        ans.push_back(s);
+        return;
+    }
+    if(index==0) {
+        s[0] = '0';
+        utils(1, N, s, ans);
+        s[0] = '1';
+        utils(1, N, s, ans);
+    }
+    else if(s[index-1]=='1') {
+        s[index] = '0';
+        utils(index+1, N, s, ans);
+    }
+    else {
+        s[index] = '0';
+        utils(index+1, N, s, ans);
+        s[index] = '1';
+        utils(index+1, N, s, ans);
+    }
+}
+
+vector<string> generateString(int N) {
+    vector<string> ans;
+    string s(N, '#');
+    utils(0, N, s, ans);
+    return ans;
+}
