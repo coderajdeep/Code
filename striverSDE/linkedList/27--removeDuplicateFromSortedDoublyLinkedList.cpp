2d8868044c71faa110f33e6a47b0bb8aa912e01c
coderajdeep <coder.rajdeep@gmail.com>
2024-04-08 08:20:57 +0530

    Remove duplicate from sorted doubly linkedlist

--- striverSDE/linkedList/27--removeDuplicateFromSortedDoublyLinkedList.cpp
+++ striverSDE/linkedList/27--removeDuplicateFromSortedDoublyLinkedList.cpp
@@ -4,24 +4,18 @@
 // Space complexity O(1)
 Node * removeDuplicates(Node *head)
 {
-    while(head && head->next && head->data==head->next->data) {
-        head = head->next;
-    }
-    if(!head || !head->next) {
-        return head;
-    }
-    Node *root = head;
-    while(head && head->next) {
-        if(head->data == head->next->data) {
-            Node *temp = head->next;
-            head->next = temp->next;
-            if(temp->next) {
-                temp->next->prev = head;
+    if(!head || !head->next) return head;
+    Node *curr = head;
+    while(curr && curr->next) {
+        if(curr->data == curr->next->data) {
+            curr->next = curr->next->next;
+            if(curr->next) {
+                curr->next->prev = curr;
             }
         }
         else {
-            head = head->next;
+            curr = curr->next;
         }
     }
-    return root;
+    return head;
 }
