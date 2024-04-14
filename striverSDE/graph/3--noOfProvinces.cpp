e39a232b7c1dc67c32515dcc9c79d60f3880b643
coderajdeep <coder.rajdeep@gmail.com>
2024-04-14 14:43:18 +0530

    Number of provinces

--- striverSDE/graph/3--noOfProvinces.cpp
+++ striverSDE/graph/3--noOfProvinces.cpp
@@ -1,10 +1,27 @@
 // Number of provinces
 // Time complexity O(V+E)
+
 void dfs(int u, vector<vector<int>>& graph, vector<bool> &visited) {
     visited[u] = true;
     for(int v : graph[u]) {
         if(!visited[v]) {
             dfs(v, graph, visited);
+        }
+    }
+}
+
+void bfs(int u, vector<vector<int>>& graph, vector<bool> &visited) {
+    queue<int> q;
+    q.push(u);
+    visited[u] = true;
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for(int v : graph[u]) {
+            if(!visited[v]) {
+                visited[v] = true;
+                q.push(v);
+            }
         }
     }
 }
@@ -15,7 +32,7 @@ vector<vector<int>> buildGraph(vector<vector<int>> &roads, int n) {
         for(int j=0; j<n; ++j) {
             // i!=j
             // this is for self loop prevention
-            if(i!=j && roads[i][j]==1) {
+            if(i != j && roads[i][j]==1) {
                 graph[i].push_back(j);
             }
         }
@@ -30,7 +47,8 @@ int findNumOfProvinces(vector<vector<int>> &roads, int n) {
     for(int i=0; i<n; ++i) {
         if(!visited[i]) {
             ++count;
-            dfs(i, graph, visited);
+            bfs(i, graph, visited);
+            // dfs(i, graph, visited);
         }
     }
     return count;
