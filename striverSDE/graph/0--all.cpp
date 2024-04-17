8d7aad6e3df997cd3ceb415b3f6aec79070b1034
coderajdeep <coder.rajdeep@gmail.com>
2024-04-17 20:14:09 +0530

    Added 01 matrix

--- striverSDE/graph/0--all.cpp
+++ striverSDE/graph/0--all.cpp
@@ -17,7 +17,17 @@
 // Number of provinces -- leetcode 547
 
 // 7
+// *** Multisource dfs
+// Little optimization : count fresh oranges and fresh to rotten oranges conversion
 // Rotting Oranges -- leetcode 994
 
 // 8
+// *** if the given cell color and converted color is the same then it may go to infinite loop if not handled properly
 // Flood fill algorithm -- leetcode 733
+
+// 9 
+// *** Using DFS is tricky
+// Detect cycle in undirected graph -- Using BSF & DSF
+
+// 10
+// 0 1 Matrix -- leetcode 542
