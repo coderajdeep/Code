5bf000a8ea2853302fc5ac15b9c7a4cf94ccc642
Rajdeep <coder.rajdeep@gmail.com>
2024-06-01 16:36:34 +0530

    Construct unique tree from inorder and postorder binary tree

--- striverSDE/binaryTree/29--binaryTreeFromInorderPostorder--106.cpp
+++ striverSDE/binaryTree/29--binaryTreeFromInorderPostorder--106.cpp
@@ -28,27 +28,22 @@ TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
 // Space complexity  O(n)
 
 TreeNode* getTree(vector<int>& inorder, vector<int>& postorder, unordered_map<int, int> &inHash, int inStart, int inEnd, int &postEnd) {
-    if(inStart>inEnd) return nullptr;
-    TreeNode* root = new TreeNode(postorder[postEnd--]);
-    int inIndex;
-    for(int i=inStart; i<=inEnd; ++i) {
-        if(inorder[i]==root->val) {
-            inIndex = i;
-            break;
-        }
+    if(inStart > inEnd) {
+        return nullptr;
     }
-    root->right = getTree(inorder, postorder, inHash, inIndex+1, inEnd, postEnd);
-    root->left = getTree(inorder, postorder, inHash, inStart, inIndex-1, postEnd);
+    int rootValue = postorder[postEnd--];
+    TreeNode *root = new TreeNode(rootValue);
+    int inRootIndex = inHash[rootValue];
+    root->right = getTree(inorder, postorder, inHash, inRootIndex+1, inEnd, postEnd);
+    root->left = getTree(inorder, postorder, inHash, inStart, inRootIndex-1, postEnd);
     return root;
 }
 TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-    int inStart = 0;
-    int inEnd = inorder.size()-1;
-    int postEnd = postorder.size()-1;
+    int n = inorder.size();
     unordered_map<int, int> inHash;
-    for(int i=inStart; i<=inEnd; ++i) {
+    for(int i=0; i<n; ++i) {
         inHash[inorder[i]] = i;
     }
-    TreeNode* root = getTree(inorder, postorder, inHash, inStart, inEnd, postEnd);
-    return root;
+    int inStart = 0, inEnd = n-1, postEnd = n-1;
+    return getTree(inorder, postorder, inHash, inStart, inEnd, postEnd);
 }
