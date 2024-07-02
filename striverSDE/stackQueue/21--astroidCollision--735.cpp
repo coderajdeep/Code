a4dfb0d9ccb5a9240ca8253e59b46462ab72eaac
Rajdeep <coder.rajdeep@gmail.com>
2024-07-02 20:48:07 +0530

    Astroid collision

--- striverSDE/stackQueue/21--astroidCollision--735.cpp
+++ striverSDE/stackQueue/21--astroidCollision--735.cpp
@@ -6,17 +6,21 @@ vector<int> asteroidCollision(vector<int>& asteroids) {
     int n = asteroids.size();
     stack<int> stk;
     for(int i=0; i<n; ++i) {
+        // need to insert if stack is empty or element is positive
         if(stk.empty() || asteroids[i] > 0) {
             stk.push(asteroids[i]);
         }
         else {
             // asteroids[i] is negative
+            // destroy all of the astroid which have lower value than the current astroid
             while(!stk.empty() && stk.top()>0 && stk.top() < abs(asteroids[i])) {
                 stk.pop();
             }
+            // if stk top and current astroid is equal then both will destroy
             if(!stk.empty() && stk.top()>0 && stk.top() == abs(asteroids[i])) {
                 stk.pop();
             }
+            // if all of the astroid has been destroid or astroid is moving to oposite direction
             else if(stk.empty() || stk.top() < 0) {
                 stk.push(asteroids[i]);
             }
