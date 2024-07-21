2d1ad1fe47325b08ef13b9cf83dba4838854195a
Rajdeep <coder.rajdeep@gmail.com>
2024-07-21 13:16:07 +0530

    Is BST using inOrder traversal

--- striverSDE/BinarySearchTree/9--isBST--98.cpp
+++ striverSDE/BinarySearchTree/9--isBST--98.cpp
@@ -13,3 +13,35 @@ bool isBSTValid(TreeNode *root, long leftValue, long rightValue) {
 bool isValidBST(TreeNode* root) {
     return isBSTValid(root, LONG_MIN, LONG_MAX);
 }
+
+
+// Uisng iterative method (level order traversal)
+// Time complexity O(n)
+// Space complexity O(w) : w --> Max node in one level
+struct Node {
+    TreeNode *node;
+    long leftValue, rightValue;
+    Node(TreeNode *node, long leftValue, long rightValue) {
+        Node::node = node;
+        Node::leftValue = leftValue;
+        Node::rightValue = rightValue;
+    }
+};
+bool isValidBST(TreeNode* root) {
+    if(!root) return true;
+    queue<Node> q;
+    q.push({root, LONG_MIN, LONG_MAX});
+    while(!q.empty()) {
+        Node curr = q.front();
+        q.pop();
+        int nodeValue = curr.node->val;
+        if(nodeValue<=curr.leftValue || nodeValue>=curr.rightValue) return false;
+        if(curr.node->left) {
+            q.push({curr.node->left, curr.leftValue, nodeValue});
+        }
+        if(curr.node->right) {
+            q.push({curr.node->right, nodeValue, curr.rightValue});
+        }
+    }
+    return true;
+}
