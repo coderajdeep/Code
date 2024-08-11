1d79d9da07dcc3012d440d4fa8199cd9176c7acf
Rajdeep <coder.rajdeep@gmail.com>
2024-08-11 10:59:11 +0530

    Update min coin

--- striverSDE/greedy/0--all.cpp
+++ striverSDE/greedy/0--all.cpp
@@ -1,5 +1,11 @@
 // 1
 // Assign cookies -- leetcode 455
 
-// 2
+// 2 **
+// Need to sort the array based on per unit value
 // Fractional Knapsack -- Geeks
+
+// 3 **
+// Minimum coin -- Geeks
+// array is sorted
+// arr[i] + arr[j] < arr[k] (i, j < k && i != j)
