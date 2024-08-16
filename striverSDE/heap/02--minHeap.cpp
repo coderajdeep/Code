02286caaf6a742b9307676486bf24a46b791de9c
Rajdeep <coder.rajdeep@gmail.com>
2024-08-16 21:58:05 +0530

    Added min heap

--- striverSDE/heap/02--minHeap.cpp
+++ striverSDE/heap/02--minHeap.cpp
@@ -135,3 +135,63 @@ public:
         }
     }
 };
+
+
+// Geeks
+// Binary Heap operation
+//Function to extract minimum value in heap and then to store 
+//next minimum value at first index.
+int MinHeap::extractMin() 
+{
+    if(heap_size==0) return -1;
+    int ans = harr[0];
+    swap(harr[0], harr[heap_size-1]);
+    --heap_size;
+    MinHeapify(0);
+    return ans;
+}
+
+//Function to delete a key at ith index.
+void MinHeap::deleteKey(int i)
+{
+    if(heap_size==0 || i>=heap_size) return;
+    decreaseKey(i, INT_MIN);
+    extractMin();
+}
+
+//Function to insert a value in Heap.
+void MinHeap::insertKey(int k) 
+{
+    harr[heap_size++] = k;
+    int index = heap_size - 1;
+    while(index>0 && harr[parent(index)]>harr[index]) {
+        swap(harr[parent(index)], harr[index]);
+        index = parent(index);
+    }
+}
+
+//Function to change value at ith index and store that value at first index.
+void MinHeap::decreaseKey(int i, int new_val) 
+{
+    harr[i] = new_val;
+    while (i != 0 && harr[parent(i)] > harr[i]) {
+        swap(harr[i], harr[parent(i)]);
+        i = parent(i);
+    }
+}
+
+/* You may call below MinHeapify function in
+   above codes. Please do not delete this code
+   if you are not writing your own MinHeapify */
+void MinHeap::MinHeapify(int i) 
+{
+    int l = left(i);
+    int r = right(i);
+    int smallest = i;
+    if (l < heap_size && harr[l] < harr[i]) smallest = l;
+    if (r < heap_size && harr[r] < harr[smallest]) smallest = r;
+    if (smallest != i) {
+        swap(harr[i], harr[smallest]);
+        MinHeapify(smallest);
+    }
+}
