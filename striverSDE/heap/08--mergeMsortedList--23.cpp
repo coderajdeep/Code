8a84766dd447bf265bdb1c562b27dcfc3138f5a7
Rajdeep <coder.rajdeep@gmail.com>
2024-08-17 12:59:51 +0530

    Merge M sorted list

--- striverSDE/heap/08--mergeMsortedList--23.cpp
+++ striverSDE/heap/08--mergeMsortedList--23.cpp
@@ -1,4 +1,5 @@
-// Merge M sorted List -- 23
+// Merge M sorted List -- leetcode 23
+// Solution 1
 // Best approach
 // Time complexity O(MlogM) + O(N*M*LogM)
 ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -22,28 +23,29 @@ ListNode* mergeKLists(vector<ListNode*>& lists) {
     return head->next;
 }
 
-
-// Brute force approach
-// Time complexity O(N*M) + O(N*M*log(N*M)) + O(N*M)
-// Space complexity O(N*M)
+// Similar to solution 1
 ListNode* mergeKLists(vector<ListNode*>& lists) {
-    vector<int> nums;
+    priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
     for(ListNode* list : lists) {
-        while(list) {
-            nums.push_back(list->val);
-            list = list->next;
+        if(list) {
+            pq.push({list->val, list});
         }
     }
-    sort(nums.begin(), nums.end());
-    ListNode* dummy = new ListNode(0);
-    ListNode* head = dummy;
-    for(int num : nums) {
-        dummy->next = new ListNode(num);
-        dummy = dummy->next;
+    ListNode *root = new ListNode(0);
+    ListNode *curr = root;
+    while(!pq.empty()) {
+        pair<int, ListNode*> node = pq.top();
+        pq.pop();
+        curr->next = (node.second);
+        curr = curr->next;
+        if((node.second)->next) {
+            pq.push({(node.second)->next->val, (node.second)->next});
+        }
     }
-    return head->next;
+    return root->next;
 }
 
+// Solution 2
 // Using merge 2 list utility function
 // Time complexity O(M*N*N)
 // Space complexity O(1)
@@ -76,3 +78,25 @@ public:
         return head;
     }
 };
+
+// Solution 3
+// Brute force approach
+// Time complexity O(N*M) + O(N*M*log(N*M)) + O(N*M)
+// Space complexity O(N*M)
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    vector<int> nums;
+    for(ListNode* list : lists) {
+        while(list) {
+            nums.push_back(list->val);
+            list = list->next;
+        }
+    }
+    sort(nums.begin(), nums.end());
+    ListNode* dummy = new ListNode(0);
+    ListNode* head = dummy;
+    for(int num : nums) {
+        dummy->next = new ListNode(num);
+        dummy = dummy->next;
+    }
+    return head->next;
+}
