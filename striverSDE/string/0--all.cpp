f4a9cdc02898d7524f30721ed0e900fa1aed5657
Rajdeep Mallick <coder.rajdeep@gmail.com>
2024-08-28 19:11:21 +0530

    String simple complete

--- striverSDE/string/0--all.cpp
+++ striverSDE/string/0--all.cpp
@@ -1,19 +1,22 @@
-// 1
+// 1 ***
+// Remove outer parentheses -- leetcode 1021
+
+// 1 **
 // Reverse Words in a string - leetcode 151
 
-// 2
+// 2 **
 // Valid Anagram -- leetcode 242
 
 // 3
 // Largest odd number in string -- leetcode 1903
 
-// 4
+// 4 *
 // Largest common prefix of all strings -- leetcode 14
 
-// 5
+// 5 **
 // Is two string are isomorphic string -- leetcode 205
 
-// 6
+// 6 ***
 // Check if a string is rotation of another -- leetcode 796
 
 // Q1
